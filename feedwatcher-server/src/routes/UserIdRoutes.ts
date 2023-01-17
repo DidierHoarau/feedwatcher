18a556096b5e2302e41e324d1fde310a5d1a8297
Didier Hoarau <4222718+DidierHoarau@users.noreply.github.com>
2023-01-17 23:54:11 +0800

    WIP

--- feedwatcher-server/src/routes/UserIdRoutes.ts
+++ feedwatcher-server/src/routes/UserIdRoutes.ts
@@ -9,84 +9,84 @@ let usersData: UsersData;
 
 export class UserIdRoutes {
   //
-  constructor(usersDataIn: UsersData) {
-    usersData = usersDataIn;
-  }
+  // constructor(usersDataIn: UsersData) {
+  //   usersData = usersDataIn;
+  // }
 
-  public async getRoutes(fastify: FastifyInstance): Promise<void> {
-    //
-    interface GetUserRequest extends RequestGenericInterface {
-      Params: {
-        userId: string;
-      };
-    }
-    fastify.get<GetUserRequest>("/", async (req, res) => {
-      Auth.mustBeAuthenticated(req, res);
-      const user = await usersData.get(
-        StandardTracer.getSpanFromRequest(req),
-        req.params.userId
-      );
-      if (!user) {
-        return res.status(404).send({ error: "Not Found" });
-      }
-      delete user.passwordEncrypted;
-      res.status(201).send(user);
-    });
+  // public async getRoutes(fastify: FastifyInstance): Promise<void> {
+  //   //
+  //   interface GetUserRequest extends RequestGenericInterface {
+  //     Params: {
+  //       userId: string;
+  //     };
+  //   }
+  //   fastify.get<GetUserRequest>("/", async (req, res) => {
+  //     Auth.mustBeAuthenticated(req, res);
+  //     const user = await usersData.get(
+  //       StandardTracer.getSpanFromRequest(req),
+  //       req.params.userId
+  //     );
+  //     if (!user) {
+  //       return res.status(404).send({ error: "Not Found" });
+  //     }
+  //     delete user.passwordEncrypted;
+  //     res.status(201).send(user);
+  //   });
 
-    interface PutUserRequest extends RequestGenericInterface {
-      Params: {
-        userId: string;
-      };
-      Body: {
-        name: string;
-        password: string;
-      };
-    }
-    fastify.put<PutUserRequest>("/", async (req, res) => {
-      Auth.mustBeAuthenticated(req, res);
-      const user = await usersData.get(
-        StandardTracer.getSpanFromRequest(req),
-        req.params.userId
-      );
-      if (!user) {
-        return res.status(404).send({ error: "Not Found" });
-      }
-      const userUpddate = new User();
-      if (!req.body.name) {
-        return res.status(400).send({ error: "Missing: Name" });
-      }
-      userUpddate.name = req.body.name;
-      if (req.body.password) {
-        await UserPassword.setPassword(
-          StandardTracer.getSpanFromRequest(req),
-          userUpddate,
-          req.body.password
-        );
-      }
-      await usersData.update(
-        StandardTracer.getSpanFromRequest(req),
-        user.id,
-        userUpddate
-      );
-      res.status(201).send({});
-    });
+  //   interface PutUserRequest extends RequestGenericInterface {
+  //     Params: {
+  //       userId: string;
+  //     };
+  //     Body: {
+  //       name: string;
+  //       password: string;
+  //     };
+  //   }
+  //   fastify.put<PutUserRequest>("/", async (req, res) => {
+  //     Auth.mustBeAuthenticated(req, res);
+  //     const user = await usersData.get(
+  //       StandardTracer.getSpanFromRequest(req),
+  //       req.params.userId
+  //     );
+  //     if (!user) {
+  //       return res.status(404).send({ error: "Not Found" });
+  //     }
+  //     const userUpddate = new User();
+  //     if (!req.body.name) {
+  //       return res.status(400).send({ error: "Missing: Name" });
+  //     }
+  //     userUpddate.name = req.body.name;
+  //     if (req.body.password) {
+  //       await UserPassword.setPassword(
+  //         StandardTracer.getSpanFromRequest(req),
+  //         userUpddate,
+  //         req.body.password
+  //       );
+  //     }
+  //     await usersData.update(
+  //       StandardTracer.getSpanFromRequest(req),
+  //       user.id,
+  //       userUpddate
+  //     );
+  //     res.status(201).send({});
+  //   });
 
-    interface DeleteUserRequest extends RequestGenericInterface {
-      Params: {
-        userId: string;
-      };
-    }
-    fastify.delete<DeleteUserRequest>("/", async (req, res) => {
-      Auth.mustBeAuthenticated(req, res);
-      const user = await usersData.get(
-        StandardTracer.getSpanFromRequest(req),
-        req.params.userId
-      );
-      if (!user) {
-        return res.status(404).send({ error: "Not Found" });
-      }
-      await usersData.delete(StandardTracer.getSpanFromRequest(req), user.id);
-      res.status(201).send({});
-    });
+  //   interface DeleteUserRequest extends RequestGenericInterface {
+  //     Params: {
+  //       userId: string;
+  //     };
+  //   }
+  //   fastify.delete<DeleteUserRequest>("/", async (req, res) => {
+  //     Auth.mustBeAuthenticated(req, res);
+  //     const user = await usersData.get(
+  //       StandardTracer.getSpanFromRequest(req),
+  //       req.params.userId
+  //     );
+  //     if (!user) {
+  //       return res.status(404).send({ error: "Not Found" });
+  //     }
+  //     await usersData.delete(StandardTracer.getSpanFromRequest(req), user.id);
+  //     res.status(201).send({});
+  //   });
   }
 }
