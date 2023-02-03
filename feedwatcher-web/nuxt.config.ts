d8056aaf5d982f6a2fbb0dd2714559075644003d
Didier Hoarau <4222718+DidierHoarau@users.noreply.github.com>
2023-02-03 13:15:11 +0800

    WIP

--- feedwatcher-web/nuxt.config.ts
+++ feedwatcher-web/nuxt.config.ts
@@ -5,15 +5,17 @@ export default defineNuxtConfig({
       charset: "utf-16",
       viewport: "width=device-width, initial-scale=1",
       title: "FeedWatcher",
-      meta: [{ name: "description", content: "FeedWatcher" }],
+      meta: [
+        { name: "description", content: "FeedWatcher" },
+        { name: "theme-color", content: "#212121" },
+      ],
       link: [
+        { rel: "manifest", href: "/manifest.json" },
+        { rel: "icon", href: "/icon.png" },
         { rel: "stylesheet", href: "/styles.css" },
         { rel: "stylesheet", href: "https://unpkg.com/@picocss/pico@latest/css/pico.min.css" },
         { rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css" },
       ],
     },
-  },
-  dayjs: {
-    plugins: ["relativeTime"],
   },
 });
