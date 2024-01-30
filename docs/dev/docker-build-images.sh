5eae6a57c11f714cb6c1ecc6c69aed061cba7de6
Didier Hoarau <4222718+DidierHoarau@users.noreply.github.com>
2024-01-31 06:39:52 +0800

    WIP

--- docs/dev/docker-build-images.sh
+++ docs/dev/docker-build-images.sh
@@ -8,4 +8,7 @@ cd ${REPO_DIR}
 SERVICE_NAME="feedwatcher"
 SERVICE_VERSION=$(cat ${REPO_DIR}/package.json | jq -r '.version')
 echo "Building ${SERVICE_NAME}/${SERVICE_VERSION}"
-docker build -f Dockerfile -t didierhoarau/${SERVICE_NAME}:${SERVICE_VERSION} .
+docker buildx build \
+  --platform linux/arm/v7,linux/arm64/v8,linux/amd64 \
+  -f Dockerfile \
+  -t didierhoarau/${SERVICE_NAME}:${SERVICE_VERSION} .
