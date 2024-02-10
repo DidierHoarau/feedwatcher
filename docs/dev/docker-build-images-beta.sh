256f64c416673a5771131da159438502378e31e4
Didier Hoarau <4222718+DidierHoarau@users.noreply.github.com>
2024-02-11 07:41:11 +0800

    Docker Multi Architecture (#62)
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP

--- docs/dev/docker-build-images-beta.sh
+++ docs/dev/docker-build-images-beta.sh
@@ -5,18 +5,16 @@ set -e
 REPO_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
 cd ${REPO_DIR}
 
-function tagAndPush {
-    IMAGE_BASE=${1}
-    IMAGE_TARGET=${2}
-    docker tag ${IMAGE_BASE} ${IMAGE_TARGET}
-    docker push ${IMAGE_TARGET}
-}
-
 SERVICE_NAME="feedwatcher"
 SERVICE_VERSION=$(cat ${REPO_DIR}/package.json | jq -r '.version')
 SERVICE_VERSION_MAJOR=$(cat ${REPO_DIR}/package.json | grep \"version\" | cut -f4 -d"\"" | cut -f1 -d".")
 SERVICE_VERSION_MINOR=$(cat ${REPO_DIR}/package.json | grep \"version\" | cut -f4 -d"\"" | cut -f1-2 -d".")
-    
-echo "Publishing ${SERVICE_NAME}/${SERVICE_VERSION}/${SERVICE_VERSION_MAJOR}/${SERVICE_VERSION_MINOR} - ${OS_VARIANT} - ${TAG}"
 
-tagAndPush didierhoarau/feedwatcher:${SERVICE_VERSION} didierhoarau/feedwatcher:beta
+echo "Building ${SERVICE_NAME}/${SERVICE_VERSION}"
+
+docker buildx build \
+  --platform linux/arm64/v8,linux/amd64 \
+   --push \
+  -f Dockerfile \
+  -t didierhoarau/${SERVICE_NAME}:beta \
+  .
