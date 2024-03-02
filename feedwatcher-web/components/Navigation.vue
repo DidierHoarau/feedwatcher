273bc86df3906e03e69b8465cdf383466843163f
Didier Hoarau <4222718+DidierHoarau@users.noreply.github.com>
2024-03-02 14:47:00 +0800

    WIP

--- feedwatcher-web/components/Navigation.vue
+++ feedwatcher-web/components/Navigation.vue
@@ -7,16 +7,24 @@
     </ul>
     <ul class="menu-links">
       <li v-if="authenticationStore.isAuthenticated">
-        <NuxtLink to="/sources"><i class="bi bi-rss-fill"></i></NuxtLink>
+        <NuxtLink to="/sources" :class="activeRoute == 'sources' ? 'active' : 'inactive'"
+          ><i class="bi bi-rss-fill"></i
+        ></NuxtLink>
       </li>
       <li v-if="authenticationStore.isAuthenticated">
-        <NuxtLink to="/bookmarks"><i class="bi bi-bookmark-check-fill"></i></NuxtLink>
+        <NuxtLink to="/bookmarks" :class="activeRoute == 'bookmarks' ? 'active' : 'inactive'"
+          ><i class="bi bi-bookmark-check-fill"></i
+        ></NuxtLink>
       </li>
       <li v-if="authenticationStore.isAuthenticated">
-        <NuxtLink to="/rules"><i class="bi bi-robot"></i></NuxtLink>
+        <NuxtLink to="/rules" :class="activeRoute == 'rules' ? 'active' : 'inactive'"
+          ><i class="bi bi-robot"></i
+        ></NuxtLink>
       </li>
       <li>
-        <NuxtLink to="/users"><i class="bi bi-person-circle"></i></NuxtLink>
+        <NuxtLink to="/users" :class="activeRoute == 'users' ? 'active' : 'inactive'"
+          ><i class="bi bi-person-circle"></i
+        ></NuxtLink>
       </li>
     </ul>
   </nav>
@@ -32,7 +40,18 @@ import axios from "axios";
 import Config from "~~/services/Config.ts";
 
 export default {
+  watch: {
+    $route(to, from) {
+      this.routeUpdated(to);
+    },
+  },
+  data() {
+    return {
+      activeRoute: "",
+    };
+  },
   async created() {
+    this.routeUpdated(this.$route);
     if (await AuthenticationStore().ensureAuthenticated()) {
       setTimeout(async () => {
         // Renew session tocken
@@ -45,6 +64,11 @@ export default {
     }
     UserProcessorInfoStore().check();
   },
+  methods: {
+    routeUpdated(newRoute) {
+      this.activeRoute = newRoute.fullPath.split("/")[1];
+    },
+  },
 };
 </script>
 
@@ -52,5 +76,11 @@ export default {
 .menu-links li {
   padding-right: 1em;
   font-size: 1.2em;
+}
+.menu-links .inactive {
+  opacity: 0.3;
+}
+.menu-links .active {
+  color: #3cabff;
 }
 </style>
