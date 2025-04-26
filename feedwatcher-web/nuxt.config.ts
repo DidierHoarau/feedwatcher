93a71499f2a39d1017fb73907360d3da8bf74628
Didier Hoarau <4222718+DidierHoarau@users.noreply.github.com>
2025-04-26 16:30:01 +0800

    wip

--- feedwatcher-web/nuxt.config.ts
+++ feedwatcher-web/nuxt.config.ts
@@ -19,7 +19,7 @@ export default defineNuxtConfig({
       ],
     },
   },
-  modules: ["@pinia/nuxt", "@vite-pwa/nuxt"],
+  modules: ["@pinia/nuxt", "@vite-pwa/nuxt", "~/assets/css/main.css"],
   imports: {
     dirs: ["./stores"],
   },
