0315bce4e19c278f6bdc5ba7edc3d35b9f0c609e
Didier Hoarau <4222718+DidierHoarau@users.noreply.github.com>
2025-04-26 16:30:30 +0800

    wip

--- feedwatcher-web/nuxt.config.ts
+++ feedwatcher-web/nuxt.config.ts
@@ -14,8 +14,6 @@ export default defineNuxtConfig({
       link: [
         { rel: "icon", href: "/icon.png" },
         { rel: "stylesheet", href: "/styles.css" },
-        { rel: "stylesheet", href: "https://unpkg.com/@picocss/pico@latest/css/pico.min.css" },
-        { rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css" },
       ],
     },
   },
